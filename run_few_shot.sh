62a2faace66dd27f58a99ea0f8217c94381e3dfb
dawn0815 <57930559+dawn0815@users.noreply.github.com>
2023-09-05 20:23:00 +0800

    Update run_few_shot.sh

--- run_few_shot.sh
+++ run_few_shot.sh
@@ -1,12 +1,14 @@
 train_emotion='./datasets/path_to_emotion_fewshot_train_dir'
 test_emotion='./datasets/path_to_emotion_fewshot_test_dir'
 
+checkpoint_saved='./checkpoint_saved/finetune'  #dir to the model weights (checkpoint)
+
 python finetune.py \
   --dataset emotion_few_train ${train_emotion} \ 
   --dataset emotion_few_test ${test_emotion} \   
   --checkpoint_dir ./checkpoint_saved \ 
   --model_config ./config/pretrain_base.json \ 
-  --checkpoint ./checkpoint_saved/finetune \ 
+  --checkpoint ${checkpoint_saved} \ 
   --log_dir ./logs \ 
   --validate_loss --amp \ 
   --continue_training
